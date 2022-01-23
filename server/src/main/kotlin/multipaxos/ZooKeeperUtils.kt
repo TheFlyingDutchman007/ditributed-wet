9b9cbeace2f7cc4ba18ca1e1d1e8d7592d4f11a7
Daniel_Meents <7707daniel@gmail.com>
2022-01-23 22:34:50 +0200

    can run multiple rest APIs

--- server/src/main/kotlin/multipaxos/ZooKeeperUtils.kt
+++ server/src/main/kotlin/multipaxos/ZooKeeperUtils.kt
@@ -6,8 +6,13 @@ import kotlinx.coroutines.runBlocking
 import org.apache.log4j.BasicConfigurator
 import org.apache.zookeeper.Watcher
 import org.apache.zookeeper.ZooKeeper
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.runApplication
+import rest_api.SpringBootBoilerplateApplication
+import rest_api.rest_api_Test
 import zookeeper.kotlin.ZooKeeperKt
 import zookeeper.kotlin.ZookeeperKtClient
+import java.util.*
 
 fun makeConnectionString(sockets: List<Pair<String, Int>>) =
     sockets.joinToString(separator = ",") { (hostname, port) ->
@@ -41,7 +46,14 @@ fun mainWith(args:Array<String> = emptyArray(), the_main: MainFunction) = runBlo
 
     val zkSockets = (1..3).map { Pair("127.0.0.1", 2180 + it) }
     val zkConnectionString = makeConnectionString(zkSockets)
-
+    /*val app = SpringApplication()
+    app.setDefaultProperties(Collections.singletonMap("server.port", "8070") as Map<String, Any>?);
+    app.run()*/
+    //runApplication<SpringBootBoilerplateApplication>(*args)
+    val id = args[0]
+    val restPort = id.toInt() + 100
+    System.setProperty("server.port", restPort.toString())
+    runApplication<SpringBootBoilerplateApplication>(*args)
     withZooKeeper(zkConnectionString) {
         the_main(args, it)
     }
